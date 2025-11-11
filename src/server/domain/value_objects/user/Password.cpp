cf280f34b872a5a2333cce28bb9819037b8e6dc8
Killian Pluenet <killian.pluenet@epitech.eu>
2025-11-11 22:02:21 +0100

    FEAT(domain): ajout Value Objects User (UserId, Username, Password) [RT-00]

---
+++ src/server/domain/value_objects/user/Password.cpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype [WSL: Ubuntu]
+** File description:
+** Password
+*/
+
+#include "domain/value_objects/user/Password.hpp"
+
+namespace domain::value_objects::user {
+    void Password::validate(const std::string &password) {
+        if (password.length() < 6) {
+            throw exceptions::user::PasswordException(password);
+        }
+    }
+
+    Password::Password(const std::string& password) : _passwordHash(password) {
+        validate(password);
+    }
+
+    std::string Password::value() const {
+        return _passwordHash;
+    }
+
+    bool Password::operator==(const Password& other) const {
+        return _passwordHash == other._passwordHash;
+    }
+
+    bool Password::operator!=(const Password& other) const {
+        return !(*this == other);
+    }
+}
---
+++ src/server/include/domain/value_objects/user/Password.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype [WSL: Ubuntu]
+** File description:
+** Password
+*/
+
+#ifndef PASSWORD_HPP_
+#define PASSWORD_HPP_
+
+#include "domain/exceptions/user/PasswordException.hpp"
+#include <string>
+
+namespace domain::value_objects::user {
+    class Password {
+        private:
+            std::string _passwordHash;
+            void validate(const std::string &password);
+
+        public:
+            explicit Password(const std::string& password);
+            std::string value() const;
+            bool operator==(const Password& other) const;
+            bool operator!=(const Password& other) const;
+    };
+}
+
+#endif /* !PASSWORD_HPP_ */
